392489fa440ffdd1c89a5d79cf1c29e55b3c7ff7
Alexandr Ryzhenkov <alex2505@bk.ru>
2025-08-25 08:56:15 +0300

    add TABLE addressBook;

--- dump/createDocuments.sql
+++ dump/createDocuments.sql
@@ -22,15 +22,23 @@ CREATE TABLE description (
 
 CREATE TABLE worksPerformed (
                                 id INT AUTO_INCREMENT PRIMARY KEY,
-                                title VARCHAR(20) NOT NULL, # красили стены, устанавливали панели;
+                                title VARCHAR(255) NOT NULL, # красили стены, устанавливали панели;
                                 description_id INT NOT NULL,
                                 FOREIGN KEY (description_id) REFERENCES description(id)
+);
+
+CREATE TABLE addressBook (
+                       address_id INT AUTO_INCREMENT PRIMARY KEY,
+                       code VARCHAR(7) NOT NULL UNIQUE,# 125222
+                       street VARCHAR(100) NOT NULL UNIQUE,# ул. Муравская
+                       apartment VARCHAR(50) NOT NULL UNIQUE# 38Бк1
 );
 
 CREATE TABLE document (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        type VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, #Евродвушка
-                       mode VARCHAR(10) NOT NULL,
+                       mode VARCHAR(10) NOT NULL, # edit|end|start|create
+                       project_key VARCHAR(255) NOT NULL, # mitino1|mitinskii38Бк1|kronstadskii222 - for create url;
                        createDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                        userRole INT(10) NOT NULL,
                        fileName VARCHAR(100) NOT NULL UNIQUE
@@ -38,9 +46,7 @@ CREATE TABLE document (
 
 CREATE TABLE breadcrumbs (
                 breadcrumbs_id INT NOT NULL AUTO_INCREMENT,
-                projectName VARCHAR(100) NOT NULL UNIQUE,# ЖК Митинский лес
-                street VARCHAR(100) NOT NULL UNIQUE,# ул. Муравская
-                house VARCHAR(100) NOT NULL UNIQUE,# 38Бк1
+                project_title VARCHAR(100) NOT NULL UNIQUE,# ЖК Митинский лес
                 document_id INT NOT NULL,
                 FOREIGN KEY (document_id) REFERENCES document(id),
                 primary key(breadcrumbs_id, document_id)
@@ -73,10 +79,27 @@ CREATE TABLE message (
 INSERT INTO homesStaging.user (id, email, name, password, createDate, remember_me, avatar, role) VALUES (1, 'alex250555@bk.ru', 'Саша', '$2y$10$0tUwDA0PeoKDK2y.83XM3.68sCRxb8ACvfEjvZoJ3Wm9zmCKSxn9u', '2025-08-13 01:30:12', '1', '/uploads/avatars/2025/01/01/avatar-0.png', 1);
 INSERT INTO homesStaging.user (id, email, name, password, createDate, remember_me, avatar, role) VALUES (2, 'nkartashove@mail.ru', 'Natalia', '$2y$10$0tUwDA0PeoKDK2y.83XM3.68sCRxb8ACvfEjvZoJ3Wm9zmCKSxn9u', '2025-08-13 01:30:12', '1', '/uploads/avatars/2025/01/01/avatar-0.png', 1);
 
-INSERT INTO homesStaging.document (id, type, mode, createDate, userRole, fileName) VALUES (1, 'Евродвушка', 'edit', '2025-08-11 23:31:18', 1, 'invrpt-new-Kramp-250807.json');
-INSERT INTO homesStaging.document (id, type,  mode, createDate, userRole, fileName) VALUES (2, 'Евродвушка', 'end', '2025-08-11 23:31:18', 1, 'invrpt1248923-edit-Kramp-250811.json');
+INSERT INTO homesStaging.description (id, title, category,price,project_url,project_des,createDate,end_date)
+VALUES (1, 'Евродвушка' ,'Комплектация "под ключ"' ,'988 000' ,'https://t.me/homeupakovka' ,'мой канал Telegram' ,NULL,NULL);
 
-INSERT INTO homesStaging.breadcrumbs (breadcrumbs_id, projectName, street,house,document_id) VALUES (1,  'ЖК Митинский лес','ул. Муравская','38Бк1',1);
+INSERT INTO homesStaging.worksPerformed (id, title, description_id)
+VALUES (1, 'красили стены, устанавливали панели;' ,1);
+INSERT INTO homesStaging.worksPerformed (id, title, description_id)
+VALUES (2, 'меняли двери, в т.ч. входную, регулировали окна и меняли откосы;' ,1);
+INSERT INTO homesStaging.worksPerformed (id, title, description_id)
+VALUES (3, 'в ванной меняли унитаз, красили швы, меняли душевую стойку и раковину;' ,1);
+INSERT INTO homesStaging.worksPerformed (id, title, description_id)
+VALUES (4, 'бытовая техника Weissgauff, фартук на кухне демонтировали и сделали из керамогранита;' ,1);
+INSERT INTO homesStaging.worksPerformed (id, title, description_id)
+VALUES (5, 'Установка сплит системы.' ,1);
+
+INSERT INTO homesStaging.addressBook (address_id, code, street, apartment)
+VALUES (1, '125222', 'ул. Муравская', '38Бк1');
+
+INSERT INTO homesStaging.document (id, type, mode, project_key, createDate, userRole, fileName) VALUES (1, 'Евродвушка', 'edit', 'mitino1', '2025-08-11 23:31:18', 1, 'invrpt-new-Kramp-250807.json');
+INSERT INTO homesStaging.document (id, type,  mode, project_key, createDate, userRole, fileName) VALUES (2, 'Евродвушка', 'end', 'kronstadskii1', '2025-08-11 23:31:18', 1, 'invrpt1248923-edit-Kramp-250811.json');
+
+INSERT INTO homesStaging.breadcrumbs (breadcrumbs_id, project_title, document_id) VALUES (1,  'ЖК Митинский лес',1);
 
 INSERT INTO homesStaging.image (image_id, image_url, document_id) VALUES (1,  'assets/img/flats/Mitinskii-les/38/1.jpg',1);
 INSERT INTO homesStaging.image (image_id, image_url, document_id) VALUES (2,  'assets/img/flats/Mitinskii-les/38/2.png',1);
